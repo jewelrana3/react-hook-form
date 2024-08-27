5e5cbcc34cccd2442a2a6d650870f19a9e1b1668
juyel haque <juyelrana7752@gmail.com>
2024-08-26 22:01:17 -0700

    react-hook-form-crash

--- src/forms/RegisterForm.jsx
+++ src/forms/RegisterForm.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import FieldSet from "../components/FieldSet";
+
+import { useForm, Controller, useFieldArray } from "react-hook-form";
 import Field from "../components/Field";
-import { useFieldArray, useForm, Controller } from "react-hook-form";
+import FieldSet from "../components/FieldSet";
 import NumberInput from "./NumberInput";
 
-const RegisterForm = () => {
+const RegistrationForm = () => {
   const {
     register,
     handleSubmit,
@@ -14,25 +15,35 @@ const RegisterForm = () => {
   } = useForm();
 
   const { fields, append, remove } = useFieldArray({
-    control,
     name: "socials",
+    control,
   });
 
-  const onSubmit = (formData) => {
+  const submitForm = (formData) => {
     console.log(formData);
   };
 
   return (
     <div className="flex flex-col justify-center items-center">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <FieldSet label="Enter Register Details">
+      <form onSubmit={handleSubmit(submitForm)}>
+        <FieldSet label="Enter Your Basic Details">
+          <Field label="Picture" error={errors.picture}>
+            <input
+              {...register("picture", {
+                required: "Picture is required",
+              })}
+              type="file"
+              id="picture"
+              name="picture"
+            />
+          </Field>
           <Field label="Email" error={errors.email}>
             <input
-              {...register("email", { required: "Email is required" })}
+              {...register("email", {
+                required: "Email is required.",
+              })}
               className={`p-2 border box-border w-[300px] rounded-md ${
-                !!errors.email
-                  ? "border border-red-500"
-                  : "border border-gray-200"
+                !!errors.email ? "border-red-500" : "border-gray-200"
               }`}
               type="email"
               name="email"
@@ -58,6 +69,7 @@ const RegisterForm = () => {
               placeholder="Enter Password"
             />
           </Field>
+
           <Field label="Full Name" error={errors.fname}>
             <input
               {...register("fname", {
@@ -72,14 +84,16 @@ const RegisterForm = () => {
               placeholder="Enter Full Name"
             />
           </Field>
+
           <Field label="Age" error={errors.age}>
             <Controller
               name="age"
               control={control}
+              defaultValue={1}
               render={({ field: { ref, ...field } }) => (
                 <NumberInput
                   id="age"
-                  className={`p-2 border box-border w-[300px] rounded-md ${
+                  className={`p-2 border box-border w-full rounded-md ${
                     !!errors.age ? "border-red-500" : "border-gray-200"
                   }`}
                   {...field}
@@ -88,14 +102,14 @@ const RegisterForm = () => {
               rules={{
                 max: {
                   value: 100,
-                  message: "At least can be 0 and 100",
+                  message: "Age can be between 0 and 100",
                 },
               }}
             />
           </Field>
         </FieldSet>
 
-        <FieldSet label="Socials Name">
+        <FieldSet label="Enter Social Handles">
           {fields.map((field, index) => {
             return (
               <div
@@ -111,6 +125,7 @@ const RegisterForm = () => {
                     name={`socials[${index}].name`}
                   />
                 </Field>
+
                 <Field label="Social URL">
                   <input
                     className="p-2 border box-border w-full rounded-md"
@@ -120,7 +135,6 @@ const RegisterForm = () => {
                     name={`socials[${index}].url`}
                   />
                 </Field>
-
                 <button
                   className="mt-8 mr-2 text-2xl"
                   onClick={() => remove(index)}
@@ -130,17 +144,20 @@ const RegisterForm = () => {
               </div>
             );
           })}
-
           <button
             className="mt-8 text-md text-white cursor-pointer border rounded-lg bg-gray-500 p-1 m-auto"
-            onClick={() => append({ name: "", url: "" })}
+            onClick={(e) => {
+              e.preventDefault();
+              append({ name: "", url: "" });
+            }}
           >
             Add A Social Handle
           </button>
         </FieldSet>
-        {errors?.root?.random?.message}
+
+        <div>{errors?.root?.random?.message}</div>
         <Field>
-          <button className="text-md text-white cursor-pointer p-1 border rounded-lg bg-purple-500">
+          <button className="text-md text-white cursor-pointer p-1 border rounded-lg bg-purple-500 m-auto">
             Register
           </button>
         </Field>
@@ -149,4 +166,4 @@ const RegisterForm = () => {
   );
 };
 
-export default RegisterForm;
+export default RegistrationForm;
